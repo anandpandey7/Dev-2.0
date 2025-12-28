0d55323ead2a9dda8ca969ba3e7356242b5623d2
anandpandey7 <anandraj001119@gmail.com>
2025-12-28 23:44:12 +0530

    using neun db for podtgres db

--- Week-10/PostgressLearn/src/utils.ts
+++ Week-10/PostgressLearn/src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgresql://admin:admin123@localhost:5432/mydb");
+    const client = new Client("postgresql://neondb_owner:npg_Luh2r0OcTjMq@ep-winter-fire-adbpbfjh-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
     await client.connect();
     return client;
 }
