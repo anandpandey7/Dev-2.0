b2af03cfc5085ce11aac8ddbd2b3898314c530e2
anandpandey7 <anandraj001119@gmail.com>
2026-01-29 20:02:55 +0530

    making blocg website & deploying it on cloudflare using cloudflare worker

---
+++ Week-13/blog/frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+import { Blog } from "./pages/Blog";
+import { Blogs } from "./pages/Blogs";
+import { Publish } from "./pages/Publish";
+import { Profile } from "./pages/Profile";
+import { User } from "./pages/User";
+
+function App(){
+  return(
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/signup" element={<Signup/>} />
+          <Route path="/signin" element={<Signin/>} />
+          <Route path="blogs" element={<Blogs/>} />
+          <Route path="blog/:id" element={<Blog/>} />
+          <Route path="/publish" element={<Publish/>} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/user/:id" element={<User />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
